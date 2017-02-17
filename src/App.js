37092e2f8bfcc6e37a9d5f6c5daa587d865d22fd
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-17 16:31:56 +0100

    Create very basic app structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Container, Grid, Menu, Segment } from "semantic-ui-react";
 
 class App extends Component {
+  state = {
+    active: 0
+  };
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column>
+              <Menu inverted>
+                <Menu.Item active={this.state.active === 0}
+                           onClick={() => this.setState({ active: 0 })}>Home</Menu.Item>
+                <Menu.Item active={this.state.active === 1} onClick={() => this.setState({ active: 1 })}
+                           position="right">Account</Menu.Item>
+              </Menu>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width="4">
+              <Segment>
+                FILTERS
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width="12">
+              <Segment>
+                PRODUCTS
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <Segment>
+                &copy; VSHN AG
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "semantic-ui-css/semantic.css";
 
 ReactDOM.render(
   <App />,
