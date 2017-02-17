a7f7d4ddebb40d39e0e2bf16b893d4edc578348d
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-17 17:41:49 +0100

    Install react-router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { browserHistory, Route, Router } from "react-router";
 import { rootReducer } from "./state/reducers";
 import { setupStore } from "./state/store";
 import App from "./App";
@@ -8,7 +9,9 @@ import "semantic-ui-css/semantic.css";
 
 ReactDOM.render(
   <Provider store={setupStore(rootReducer)}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={App}/>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
