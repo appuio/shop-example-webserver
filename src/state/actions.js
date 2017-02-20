d31d62fdaba0995a84b60e50ce2e118c0251a961
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-20 19:02:18 +0100

    Add basic actions for api

---
+++ src/state/actions.js
@@ -0,0 +1,17 @@
+export const PRODUCTS_REQUEST = 'PRODUCTS_REQUEST';
+export const productsRequest = () => ({ type: PRODUCTS_REQUEST });
+
+export const PRODUCTS_RECEIVE = 'PRODUCTS_RECEIVE';
+export const productsReceive = (products) => ({ type: PRODUCTS_RECEIVE, products });
+
+export const PRODUCTS_FAIL = 'PRODUCTS_FAIL';
+export const productsFail = () => ({ type: PRODUCTS_FAIL });
+
+export const PRODUCT_REQUEST = 'PRODUCT_REQUEST';
+export const productRequest = (id) => ({ type: PRODUCT_REQUEST, id });
+
+export const PRODUCT_RECEIVE = 'PRODUCT_RECEIVE';
+export const productReceive = (product) => ({ type: PRODUCT_RECEIVE, product });
+
+export const PRODUCT_FAIL = 'PRODUCT_FAIL';
+export const productFail = () => ({ type: PRODUCT_FAIL });
