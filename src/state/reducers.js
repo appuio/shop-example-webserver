c8faf13a1f400b0f2f895f1322c025b9244daab1
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-20 19:17:20 +0100

    Create reducer stubs

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,1 +1,27 @@
-export const rootReducer = (state, action) => state;
+import { combineReducers } from "redux";
+import * as Actions from "./actions";
+
+const product = (state, action) => {
+  switch (action.type) {
+    case Actions.PRODUCT_REQUEST:
+    case Actions.PRODUCT_RECEIVE:
+    case Actions.PRODUCT_FAIL:
+    default:
+      return state;
+  }
+};
+
+const products = (state, action) => {
+  switch (action.type) {
+    case Actions.PRODUCTS_REQUEST:
+    case Actions.PRODUCTS_RECEIVE:
+    case Actions.PRODUCTS_FAIL:
+    default:
+      return state;
+  }
+};
+
+export const rootReducer = combineReducers({
+  products,
+  product
+});
