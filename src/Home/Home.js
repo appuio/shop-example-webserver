3e0e1d8d2be40c45005be09446b44a8dc23f4d90
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-21 10:09:00 +0100

    Add basic redux functionality

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,43 +1,58 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Card, Grid, Input, List, Segment } from "semantic-ui-react";
+import { productsRequest } from "../state/actions";
 import Product from "./Product";
 
-const Home = () => {
-  const products = [
-    { name: "Microsoft Windows 10", price: 250 },
-    { name: "Microsoft Office 2016", price: 100 },
-    { name: "Ubuntu 16.10", price: 0 },
-    { name: "NodeJS 0.10", price: 0 }
-  ];
+class Home extends React.Component {
+  componentDidMount() {
+    this.props.productsRequest();
+  }
 
-  return (
-    <Grid.Row>
-      <Grid.Column width={4}>
-        <Segment>
-          <Input fluid placeholder="Search..."/>
-          <List>
-            <List.Item>
-              <List.Icon name="folder"/>
-              <List.Content>Continuous Integration</List.Content>
-            </List.Item>
-            <List.Item>
-              <List.Icon name="folder"/>
-              <List.Content>Operating Systems</List.Content>
-            </List.Item>
-            <List.Item>
-              <List.Icon name="folder"/>
-              <List.Content>Webservers</List.Content>
-            </List.Item>
-          </List>
-        </Segment>
-      </Grid.Column>
-      <Grid.Column width={12}>
-        <Card.Group itemsPerRow={3}>
-          {products.map((product, index) => <Product key={index} index={index} {...product} />)}
-        </Card.Group>
-      </Grid.Column>
-    </Grid.Row>
-  );
-};
+  render() {
+    const products = [
+      { name: "Microsoft Windows 10", price: 250 },
+      { name: "Microsoft Office 2016", price: 100 },
+      { name: "Ubuntu 16.10", price: 0 },
+      { name: "NodeJS 0.10", price: 0 }
+    ];
 
-export default Home;
+    return (
+      <Grid.Row>
+        <Grid.Column width={4}>
+          <Segment>
+            <Input fluid placeholder="Search..."/>
+            <List>
+              <List.Item>
+                <List.Icon name="folder"/>
+                <List.Content>Continuous Integration</List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Icon name="folder"/>
+                <List.Content>Operating Systems</List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Icon name="folder"/>
+                <List.Content>Webservers</List.Content>
+              </List.Item>
+            </List>
+          </Segment>
+        </Grid.Column>
+        <Grid.Column width={12}>
+          <Card.Group itemsPerRow={3}>
+            {products.map((product, index) => <Product key={index} index={index} {...product} />)}
+          </Card.Group>
+        </Grid.Column>
+      </Grid.Row>
+    );
+  }
+}
+
+export default connect(
+  state => ({
+    products: state.products
+  }),
+  dispatch => ({
+    productsRequest: dispatch(productsRequest())
+  })
+)(Home);
