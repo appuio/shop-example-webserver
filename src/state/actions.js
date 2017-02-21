100c41ac331a3a166439e26e38123b0873acd846
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-21 11:15:30 +0100

    Install redux-observable and add some epics

--- src/state/actions.js
+++ src/state/actions.js
@@ -1,3 +1,5 @@
+import { ajax } from "rxjs/observable/dom/ajax";
+
 export const PRODUCTS_REQUEST = 'PRODUCTS_REQUEST';
 export const productsRequest = () => ({ type: PRODUCTS_REQUEST });
 
@@ -5,7 +7,7 @@ export const PRODUCTS_RECEIVE = 'PRODUCTS_RECEIVE';
 export const productsReceive = (products) => ({ type: PRODUCTS_RECEIVE, payload: products });
 
 export const PRODUCTS_FAIL = 'PRODUCTS_FAIL';
-export const productsFail = () => ({ type: PRODUCTS_FAIL });
+export const productsFail = (error) => ({ type: PRODUCTS_FAIL, payload: error });
 
 export const PRODUCT_REQUEST = 'PRODUCT_REQUEST';
 export const productRequest = (id) => ({ type: PRODUCT_REQUEST, payload: id });
@@ -14,4 +16,20 @@ export const PRODUCT_RECEIVE = 'PRODUCT_RECEIVE';
 export const productReceive = (product) => ({ type: PRODUCT_RECEIVE, payload: product });
 
 export const PRODUCT_FAIL = 'PRODUCT_FAIL';
-export const productFail = () => ({ type: PRODUCT_FAIL });
+export const productFail = (error) => ({ type: PRODUCT_FAIL, payload: error });
+
+const fetchProductsEpic = action$ =>
+  action$.ofType(PRODUCTS_REQUEST)
+    .mergeMap(action =>
+      ajax.getJSON(`https://jsonplaceholder.typicode.com/posts`)
+        .map(response => productsReceive(response))
+        .catch(error => productsFail(error))
+    );
+
+const fetchProductEpic = action$ =>
+  action$.ofType(PRODUCT_REQUEST)
+    .mergeMap(action =>
+      ajax.getJSON(`https://jsonplaceholder.typicode.com/posts/1`)
+        .map(response => productReceive(response))
+        .catch(error => productFail(error))
+    );
