1afc211b1f1c08174023c8828bd13be0343b11ec
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-21 14:51:53 +0100

    Add utils for parsing the HTTP status

---
+++ src/utils/utils.js
@@ -0,0 +1,11 @@
+export const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 400) {
+    return response;
+  } else {
+    const error = new Error(response.statusText);
+    error.response = response;
+    throw error
+  }
+};
+
+export const parseJSON = (response) => response.json;
