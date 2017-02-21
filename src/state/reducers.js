1b571d3c6c9d709f831333dda6b6d27e404c1368
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-21 09:42:02 +0100

    Setup basic state

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,27 +1,62 @@
-import { combineReducers } from "redux";
 import * as Actions from "./actions";
 
-const product = (state, action) => {
+const product = (state = {
+  loading: false,
+  data: null,
+  error: null
+}, action) => {
   switch (action.type) {
     case Actions.PRODUCT_REQUEST:
+      return { ...state, loading: true };
+
     case Actions.PRODUCT_RECEIVE:
+      return {
+        ...state,
+        loading: false,
+        data: action.payload
+      };
+
     case Actions.PRODUCT_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
+
     default:
       return state;
   }
 };
 
-const products = (state, action) => {
+const products = (state = {
+  loading: false,
+  data: null,
+  error: null
+}, action) => {
   switch (action.type) {
     case Actions.PRODUCTS_REQUEST:
+      return { ...state, loading: true };
+
     case Actions.PRODUCTS_RECEIVE:
+      return {
+        ...state,
+        loading: false,
+        data: action.payload
+      };
+
     case Actions.PRODUCTS_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
+
     default:
       return state;
   }
 };
 
-export const rootReducer = combineReducers({
+export const rootReducer = {
   products,
   product
-});
+};
