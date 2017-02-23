569364ed18223ac9bcafb71141fb7e5e499b3b77
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-23 19:29:38 +0100

    Add basic layout for the cart, install lodash

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,36 @@
 import React from "react";
-import { Grid } from "semantic-ui-react";
+import { Grid, Message, Segment, Table } from "semantic-ui-react";
+import _map from "lodash/map";
+import CartItem from "./CartItem";
 
-const Cart = () => {
+const Cart = ({ cart }) => {
   return (
     <Grid.Row>
       <Grid.Column>
-        cart
+        <Segment>
+          {
+            cart.size > 0
+              ?
+              <Table>
+                <Table.Header>
+                  <Table.HeaderCell>
+                    Name
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                    Quantity
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                    Price
+                  </Table.HeaderCell>
+                </Table.Header>
+                <Table.Body>
+                  {_map(cart.items, item => <CartItem {...item}/>)}
+                </Table.Body>
+              </Table>
+              :
+              <Message info>Your cart is currently empty...</Message>
+          }
+        </Segment>
       </Grid.Column>
     </Grid.Row>
   );
