b1ecd8bd132796b1f1646c2ad6879b6b93957c73
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-23 19:29:16 +0100

    Reimplment using an object as with uuid as identifier

--- src/redux/modules/cart.js
+++ src/redux/modules/cart.js
@@ -1,52 +1,70 @@
-import "whatwg-fetch";
+import _filter from "lodash/filter";
+import _map from "lodash/map";
+
+// utils
+// TODO: extract?
+const calculateSum = (items) => {
+  // reduce all the items in the cart to a total sum
+  return _map(items, item => item.price * item.quantity).reduce((a, b) => a + b)
+}
 
 // actions
-const ITEM_ADD = 'app/cart/ITEM_ADD';
-const ITEM_REMOVE = 'app/cart/ITEM_REMOVE';
-const ITEM_UPDATE = 'app/cart/ITEM_UPDATE';
+const ITEM_ADD = 'app/cart/ITEM_ADD'
+const ITEM_REMOVE = 'app/cart/ITEM_REMOVE'
+const ITEM_UPDATE = 'app/cart/ITEM_UPDATE'
 
 // reducer
 const reducer = (state = {
-  items: [
-    {
-      uuid: 'hehehe',
-      name: 'asasd',
-      price: 900
-    }
-  ]
+  items: {},
+  size: 0,
+  sum: 0
 }, action) => {
   switch (action.type) {
-    case ITEM_ADD:
+    case ITEM_ADD: {
+      const items = {
+        ...state.items,
+        [action.payload.item.uuid]: action.payload.item
+      }
+
       return {
         ...state,
-        items: [...state.items, action.payload.item]
+        items,
+        size: state.size + 1,
+        sum: calculateSum(items)
       };
+    }
 
-    case ITEM_REMOVE:
+    case ITEM_REMOVE: {
+      const items = _filter(state.items, item => item.uuid !== action.payload.uuid)
+
       return {
         ...state,
-        items: state.items.filter((item, index) => index !== action.payload.index)
+        items,
+        sum: calculateSum(items)
       }
+    }
 
-    case ITEM_UPDATE:
+    case ITEM_UPDATE: {
       let items = state.items;
-      items[action.payload.index].quantity = action.payload.quantity;
+      items[action.payload.uuid].quantity = action.payload.quantity
 
       return {
         ...state,
-        items
+        items,
+        size: state.size - 1,
+        sum: calculateSum(items)
       }
+    }
 
     default:
-      return state;
+      return state
   }
 };
 
 // action creators
-export const cartAddItem = (item) => ({ type: ITEM_ADD, payload: { item } });
-export const cartRemoveItem = (index) => ({ type: ITEM_REMOVE, payload: { index } });
-export const cartUpdateItem = (index, quantity) => ({ type: ITEM_UPDATE, payload: { index, quantity } });
+export const cartAddItem = (item) => ({ type: ITEM_ADD, payload: { item } })
+export const cartRemoveItem = (uuid) => ({ type: ITEM_REMOVE, payload: { uuid } })
+export const cartUpdateItem = (uuid, quantity) => ({ type: ITEM_UPDATE, payload: { uuid, quantity } })
 
 // export the reducer as default
-export default reducer;
-
+export default reducer
