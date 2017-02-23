1a4b0b8ab1ce966c26271ac32311e572afebd3b8
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-23 19:27:21 +0100

    Add basic item for the cart

---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Table } from "semantic-ui-react";
+
+const CartItem = ({ name, quantity, price }) => {
+  return (
+    <Table.Row>
+      <Table.Cell>{name}</Table.Cell>
+      <Table.Cell>{quantity}</Table.Cell>
+      <Table.Cell>{price}</Table.Cell>
+    </Table.Row>
+  );
+};
+
+export default CartItem;
