12b36a0c1b768046c4a720102ac13f8170736e83
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-24 15:35:03 +0100

    Use thunk.withExtrArgument to inject fetch into thunk action creators

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -3,6 +3,7 @@ import {routerReducer} from "react-router-redux"
 import {composeWithDevTools} from "redux-devtools-extension"
 import thunk from "redux-thunk"
 import createLogger from "redux-logger"
+import "whatwg-fetch"
 import * as reducers from "../modules/index"
 
 const logger = createLogger()
@@ -14,7 +15,7 @@ export const configureStore = () => {
       routing: routerReducer
     }),
     composeWithDevTools(
-      applyMiddleware(thunk, logger)
+      applyMiddleware(thunk.withExtraArgument({fetch}), logger)
     )
   )
 }
