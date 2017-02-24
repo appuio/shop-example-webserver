4b978b202516b9ec41768b287cbcce5a94fec7a4
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-24 17:06:09 +0100

    Dynamically calculate the sidebar options

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,35 +2,52 @@ import React from "react"
 import {Card, Dropdown, Grid, Input, Loader, Message, Segment} from "semantic-ui-react"
 import Product from "./Product"
 
-const Products = ({products: {loading, filtered, error}, applyFilter}) =>
-  <Grid.Row>
-    <Grid.Column width={4}>
-      <Segment>
-        <Input fluid placeholder="Search..."/>
-        <Dropdown placeholder="Category" fluid multiple selection options={[
-          {text: "Continuous Integration", value: "Continuous Integration"},
-          {text: "Operating Systems", value: "Operating Systems"}
-        ]} onChange={(e, {value}) => applyFilter('category', value)}/>
-        <Dropdown placeholder="License type" fluid multiple selection options={[
-          {text: "Single-User", value: "Single-User"},
-          {text: "Volume licensing", value: "Volume licensing"}
-        ]} onChange={(e, {value}) => applyFilter('licenseType', value)}/>
-        <Dropdown placeholder="Publisher" fluid multiple selection options={[
-          {text: "Gitlab", value: "Gitlab"},
-          {text: "Microsoft", value: "Microsoft"}
-        ]} onChange={(e, {value}) => applyFilter('publisher', value)}/>
-      </Segment>
-    </Grid.Column>
-    <Grid.Column width={12}>
-      {loading && <Loader active inline="centered"/>}
-      {
-        !loading &&
-        <Card.Group itemsPerRow={3}>
-          {filtered.map(item => <Product key={item.product.id} {...item} />)}
-        </Card.Group>
-      }
-      {error && <Message error>Error: {error}</Message>}
-    </Grid.Column>
-  </Grid.Row>
+const Products = ({products: {loading, filtered, filters, error}, applyFilter}) => {
+  // dynamically calculate the options to be displayed in the sidebar
+  const categoryOptions = filters.categories.map(category => ({text: category, value: category}))
+  const licenseTypeOptions = filters.licenseTypes.map(licenseType => ({text: licenseType, value: licenseType}))
+  const publisherOptions = filters.publishers.map(publisher => ({text: publisher, value: publisher}))
+
+  return (
+    <Grid.Row>
+      <Grid.Column width={4}>
+        <Segment>
+          <Input fluid placeholder="Search..."/>
+          <Dropdown
+            placeholder="Category"
+            fluid
+            multiple selection
+            options={categoryOptions}
+            onChange={(e, {value}) => applyFilter('category', value)}
+          />
+          <Dropdown
+            placeholder="License type"
+            fluid
+            multiple selection
+            options={licenseTypeOptions}
+            onChange={(e, {value}) => applyFilter('licenseType', value)}
+          />
+          <Dropdown
+            placeholder="Publisher"
+            fluid
+            multiple selection
+            options={publisherOptions}
+            onChange={(e, {value}) => applyFilter('publisher', value)}
+          />
+        </Segment>
+      </Grid.Column>
+      <Grid.Column width={12}>
+        {loading && <Loader active inline="centered"/>}
+        {
+          !loading &&
+          <Card.Group itemsPerRow={3}>
+            {filtered.map(item => <Product key={item.product.id} {...item} />)}
+          </Card.Group>
+        }
+        {error && <Message error>Error: {error}</Message>}
+      </Grid.Column>
+    </Grid.Row>
+  )
+}
 
 export default Products
