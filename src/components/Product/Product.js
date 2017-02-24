a7f6962a8f65d74196954d02babe16b00cfc07da
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-24 15:45:06 +0100

    Add a wrapping segment

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,129 +5,131 @@ const Product = ({product: {loading, data, error}, inCart, addToCart}) => {
   return (
     <Grid.Row>
       <Grid.Column>
-        {loading && <Loader active inline="centered"/>}
-        {
-          !loading && data &&
-          <Grid>
-            <Grid.Row>
-              <Grid.Column>
-                <Header as="h1">
-                  {data.product.name}
-                </Header>
-                <Label.Group size="large">
-                  <Label basic><Icon name="tag"/>{data.category.name}</Label>
-                  <Label basic><Icon name="industry"/>{data.publisher.name}</Label>
-                  <Label basic><Icon name="file text outline"/>{data.licenseType.name}</Label>
-                  <Button content="Add to cart" floated="right" icon="in cart" labelPosition="right"
-                          disabled={inCart} onClick={(e) => addToCart()}/>
-                </Label.Group>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column width={4}>
-                <Header attached="top">Pricing</Header>
-                <Segment attached><strong>{data.product.price} CHF</strong> ({data.licenseType.name})</Segment>
-              </Grid.Column>
-              <Grid.Column width={4}>
-                <Header attached="top">Description</Header>
-                <Segment attached>{data.product.description}</Segment>
-              </Grid.Column>
-              <Grid.Column width={8}>
-                <Header attached="top">Detailed specifications</Header>
-                <Segment attached>
-                  <Accordion>
-                    <Accordion.Title>
-                      <Icon name='dropdown'/>
-                      Supported operating systems
-                    </Accordion.Title>
-                    <Accordion.Content>
-                      <p>Sorry, Linux only..</p>
-                    </Accordion.Content>
-                    <Accordion.Title>
-                      <Icon name='dropdown'/>
-                      Hardware requirements
-                    </Accordion.Title>
-                    <Accordion.Content>
-                      <p>16 cores, 128GB RAM</p>
-                    </Accordion.Content>
-                  </Accordion>
-                </Segment>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column>
-                <Header attached="top">Reviews</Header>
-                <Segment attached>
-                  <Comment.Group>
-                    <Comment>
-                      <Comment.Avatar src='http://semantic-ui.com/images/avatar/small/matt.jpg'/>
-                      <Comment.Content>
-                        <Comment.Author as='a'>Matt</Comment.Author>
-                        <Comment.Metadata>
-                          <div>Today at 5:42PM</div>
-                        </Comment.Metadata>
-                        <Comment.Text>How artistic!</Comment.Text>
-                        <Comment.Actions>
-                          <Comment.Action>Reply</Comment.Action>
-                        </Comment.Actions>
-                      </Comment.Content>
-                    </Comment>
+        <Segment>
+          {loading && <Loader active inline="centered"/>}
+          {
+            !loading && data &&
+            <Grid>
+              <Grid.Row>
+                <Grid.Column>
+                  <Header as="h1">
+                    {data.product.name}
+                  </Header>
+                  <Label.Group size="large">
+                    <Label basic><Icon name="tag"/>{data.category.name}</Label>
+                    <Label basic><Icon name="industry"/>{data.publisher.name}</Label>
+                    <Label basic><Icon name="file text outline"/>{data.licenseType.name}</Label>
+                    <Button content="Add to cart" floated="right" icon="in cart" labelPosition="right"
+                            disabled={inCart} onClick={(e) => addToCart()}/>
+                  </Label.Group>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column width={4}>
+                  <Header attached="top">Pricing</Header>
+                  <Segment attached><strong>{data.product.price} CHF</strong> ({data.licenseType.name})</Segment>
+                </Grid.Column>
+                <Grid.Column width={4}>
+                  <Header attached="top">Description</Header>
+                  <Segment attached>{data.product.description}</Segment>
+                </Grid.Column>
+                <Grid.Column width={8}>
+                  <Header attached="top">Detailed specifications</Header>
+                  <Segment attached>
+                    <Accordion>
+                      <Accordion.Title>
+                        <Icon name='dropdown'/>
+                        Supported operating systems
+                      </Accordion.Title>
+                      <Accordion.Content>
+                        <p>Sorry, Linux only..</p>
+                      </Accordion.Content>
+                      <Accordion.Title>
+                        <Icon name='dropdown'/>
+                        Hardware requirements
+                      </Accordion.Title>
+                      <Accordion.Content>
+                        <p>16 cores, 128GB RAM</p>
+                      </Accordion.Content>
+                    </Accordion>
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Header attached="top">Reviews</Header>
+                  <Segment attached>
+                    <Comment.Group>
+                      <Comment>
+                        <Comment.Avatar src='http://semantic-ui.com/images/avatar/small/matt.jpg'/>
+                        <Comment.Content>
+                          <Comment.Author as='a'>Matt</Comment.Author>
+                          <Comment.Metadata>
+                            <div>Today at 5:42PM</div>
+                          </Comment.Metadata>
+                          <Comment.Text>How artistic!</Comment.Text>
+                          <Comment.Actions>
+                            <Comment.Action>Reply</Comment.Action>
+                          </Comment.Actions>
+                        </Comment.Content>
+                      </Comment>
 
-                    <Comment>
-                      <Comment.Avatar src='http://semantic-ui.com/images/avatar/small/elliot.jpg'/>
-                      <Comment.Content>
-                        <Comment.Author as='a'>Elliot Fu</Comment.Author>
-                        <Comment.Metadata>
-                          <div>Yesterday at 12:30AM</div>
-                        </Comment.Metadata>
-                        <Comment.Text>
-                          <p>This has been very useful for my research. Thanks as well!</p>
-                        </Comment.Text>
-                        <Comment.Actions>
-                          <Comment.Action>Reply</Comment.Action>
-                        </Comment.Actions>
-                      </Comment.Content>
-                      <Comment.Group>
-                        <Comment>
-                          <Comment.Avatar src='http://semantic-ui.com/images/avatar/small/jenny.jpg'/>
-                          <Comment.Content>
-                            <Comment.Author as='a'>Jenny Hess</Comment.Author>
-                            <Comment.Metadata>
-                              <div>Just now</div>
-                            </Comment.Metadata>
-                            <Comment.Text>
-                              Elliot you are always so right :)
-                            </Comment.Text>
-                            <Comment.Actions>
-                              <Comment.Action>Reply</Comment.Action>
-                            </Comment.Actions>
-                          </Comment.Content>
-                        </Comment>
-                      </Comment.Group>
-                    </Comment>
+                      <Comment>
+                        <Comment.Avatar src='http://semantic-ui.com/images/avatar/small/elliot.jpg'/>
+                        <Comment.Content>
+                          <Comment.Author as='a'>Elliot Fu</Comment.Author>
+                          <Comment.Metadata>
+                            <div>Yesterday at 12:30AM</div>
+                          </Comment.Metadata>
+                          <Comment.Text>
+                            <p>This has been very useful for my research. Thanks as well!</p>
+                          </Comment.Text>
+                          <Comment.Actions>
+                            <Comment.Action>Reply</Comment.Action>
+                          </Comment.Actions>
+                        </Comment.Content>
+                        <Comment.Group>
+                          <Comment>
+                            <Comment.Avatar src='http://semantic-ui.com/images/avatar/small/jenny.jpg'/>
+                            <Comment.Content>
+                              <Comment.Author as='a'>Jenny Hess</Comment.Author>
+                              <Comment.Metadata>
+                                <div>Just now</div>
+                              </Comment.Metadata>
+                              <Comment.Text>
+                                Elliot you are always so right :)
+                              </Comment.Text>
+                              <Comment.Actions>
+                                <Comment.Action>Reply</Comment.Action>
+                              </Comment.Actions>
+                            </Comment.Content>
+                          </Comment>
+                        </Comment.Group>
+                      </Comment>
 
-                    <Comment>
-                      <Comment.Avatar src='http://semantic-ui.com/images/avatar/small/joe.jpg'/>
-                      <Comment.Content>
-                        <Comment.Author as='a'>Joe Henderson</Comment.Author>
-                        <Comment.Metadata>
-                          <div>5 days ago</div>
-                        </Comment.Metadata>
-                        <Comment.Text>
-                          Dude, this is awesome. Thanks so much
-                        </Comment.Text>
-                        <Comment.Actions>
-                          <Comment.Action>Reply</Comment.Action>
-                        </Comment.Actions>
-                      </Comment.Content>
-                    </Comment>
-                  </Comment.Group>
-                </Segment>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        }
-        {error && <Message error>Error: {error}</Message>}
+                      <Comment>
+                        <Comment.Avatar src='http://semantic-ui.com/images/avatar/small/joe.jpg'/>
+                        <Comment.Content>
+                          <Comment.Author as='a'>Joe Henderson</Comment.Author>
+                          <Comment.Metadata>
+                            <div>5 days ago</div>
+                          </Comment.Metadata>
+                          <Comment.Text>
+                            Dude, this is awesome. Thanks so much
+                          </Comment.Text>
+                          <Comment.Actions>
+                            <Comment.Action>Reply</Comment.Action>
+                          </Comment.Actions>
+                        </Comment.Content>
+                      </Comment>
+                    </Comment.Group>
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          }
+          {error && <Message error>Error: {error}</Message>}
+        </Segment>
       </Grid.Column>
     </Grid.Row>
   )
