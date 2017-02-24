1ac5943adad0036c91be809b63368d5be50f60a1
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-24 14:46:24 +0100

    Add removeItem and updateItem to mapDispatchToProps

--- src/containers/CartContainer.js
+++ src/containers/CartContainer.js
@@ -1,10 +1,11 @@
 import React, {Component} from "react"
 import {connect} from "react-redux"
+import {cartRemoveItem, cartUpdateItem} from "../redux/modules/cart"
 import Cart from "../components/Cart/Cart"
 
 class ProductsContainer extends Component {
   render() {
-    return <Cart cart={this.props.cart}/>
+    return <Cart cart={this.props.cart} updateItem={this.props.updateItem} removeItem={this.props.removeItem}/>
   }
 }
 
@@ -12,5 +13,8 @@ export default connect(
   state => ({
     cart: state.cart
   }),
-  dispatch => ({})
+  dispatch => ({
+    removeItem: (uuid) => dispatch(cartRemoveItem(uuid)),
+    updateItem: (uuid, quantity) => dispatch(cartUpdateItem(uuid, quantity))
+  })
 )(ProductsContainer)
