dc111b8a6e1e5fd393be02a6e74405a67c20c754
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-24 13:40:33 +0100

    Add test for cart reducer and finish implementing ITEM_UPDATE

--- src/redux/modules/cart.js
+++ src/redux/modules/cart.js
@@ -1,16 +1,15 @@
-import _filter from "lodash/filter";
-import _map from "lodash/map";
+import _omitBy from "lodash/omitBy"
+import _map from "lodash/map"
+import _size from "lodash/size"
 
 // utils
-// TODO: extract?
-const calculateSum = (items) => {
+export const calculateSum = (items) => {
   // reduce all the items in the cart to a total sum
-  return _map(items, item => item.price * item.quantity).reduce((a, b) => a + b)
+  return _map(items, item => (item.price * item.quantity) || 0).reduce((a, b) => a + b)
 }
 
 // actions
 const ITEM_ADD = 'app/cart/ITEM_ADD'
-const ITEM_REMOVE = 'app/cart/ITEM_REMOVE'
 const ITEM_UPDATE = 'app/cart/ITEM_UPDATE'
 
 // reducer
@@ -29,29 +28,41 @@ const reducer = (state = {
       return {
         ...state,
         items,
-        size: state.size + 1,
+        size: _size(items),
         sum: calculateSum(items)
       };
     }
 
-    case ITEM_REMOVE: {
-      const items = _filter(state.items, item => item.uuid !== action.payload.uuid)
-
-      return {
-        ...state,
-        items,
-        sum: calculateSum(items)
-      }
-    }
-
     case ITEM_UPDATE: {
-      let items = state.items;
-      items[action.payload.uuid].quantity = action.payload.quantity
+      // if an invalid quantity has been provided, return the state
+      if (isNaN(action.payload.quantity))
+        return state;
+
+      // if a negative or zero quantity has been provided, delete the item
+      if (action.payload.quantity <= 0) {
+        const items = _omitBy(state.items, item => item.uuid === action.payload.uuid);
+
+        return {
+          ...state,
+          items,
+          size: _size(items),
+          sum: calculateSum(items)
+        }
+      }
+
+      // if a positiv quantity has been provided, update the quantity of the item
+      const items = {
+        ...state.items,
+        [action.payload.uuid]: {
+          ...state.items[action.payload.uuid],
+          quantity: action.payload.quantity
+        }
+      };
 
       return {
         ...state,
         items,
-        size: state.size - 1,
+        size: _size(items),
         sum: calculateSum(items)
       }
     }
@@ -63,8 +74,8 @@ const reducer = (state = {
 
 // action creators
 export const cartAddItem = (item) => ({ type: ITEM_ADD, payload: { item } })
-export const cartRemoveItem = (uuid) => ({ type: ITEM_REMOVE, payload: { uuid } })
 export const cartUpdateItem = (uuid, quantity) => ({ type: ITEM_UPDATE, payload: { uuid, quantity } })
+export const cartRemoveItem = (uuid) => ({type: ITEM_UPDATE, payload: {uuid, quantity: 0}})
 
 // export the reducer as default
 export default reducer
---
+++ tests/reducers/cart.test.js
@@ -0,0 +1,91 @@
+import reducer, {calculateSum, cartAddItem, cartUpdateItem} from "../../src/redux/modules/cart"
+
+const item1 = {
+  uuid: "testing123",
+  name: "testname",
+  price: 100,
+  quantity: 1
+}
+const item2 = {
+  uuid: "testing345",
+  name: "tester",
+  price: 30,
+  quantity: 2
+}
+
+const initialState = {
+  items: {},
+  size: 0,
+  sum: 0
+};
+const withOneItem = {
+  items: {
+    testing123: item1
+  },
+  size: 1,
+  sum: 100
+}
+const withTwoItems = {
+  items: {
+    testing123: item1,
+    testing345: item2
+  },
+  size: 2,
+  sum: 160
+}
+
+describe('cart - utils', () => {
+  it('should calculate the sum correctly (calculateSum)', () => {
+    // calculate the sum with valid items
+    expect(calculateSum({
+      1: {price: 7, quantity: 1},
+      2: {price: 13, quantity: 2},
+      3: {price: 0, quantity: 4}
+    })).toEqual(33)
+
+    // calculate the sum with an invalid item
+    expect(calculateSum({
+      1: {price: 20, quantity: 2},
+      2: {price: 13},
+      3: {price: 0, quantity: 4}
+    })).toEqual(40)
+  })
+});
+
+describe('cart - reducer', () => {
+
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(initialState)
+  })
+
+  it('should handle ITEM_ADD', () => {
+    // test state after one item has been added
+    expect(reducer(initialState, cartAddItem(item1))).toEqual(withOneItem)
+
+    // test state after a second item has been added
+    expect(reducer(withOneItem, cartAddItem(item2))).toEqual(withTwoItems)
+  })
+
+  it('should handle ITEM_UPDATE', () => {
+    expect(reducer(withOneItem, cartUpdateItem("testing123", 2))).toEqual({
+      items: {
+        testing123: {
+          ...item1,
+          quantity: 2
+        }
+      },
+      size: 1,
+      sum: 200
+    })
+
+    expect(reducer(withTwoItems, cartUpdateItem("testing345", 0))).toEqual({
+      items: {
+        testing123: item1
+      },
+      size: 1,
+      sum: 100
+    })
+  })
+
+})
+
