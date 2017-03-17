be3f6631178312154529ecd6f6471930f42b408d
Roland Schläfli <rolandschlaefli@gmail.com>
2017-03-17 13:32:19 +0100

    Add dropdown while signed in, add sign up button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
-import {Grid, Icon, Menu} from 'semantic-ui-react'
+import {Dropdown, Grid, Icon, Menu} from 'semantic-ui-react'
 
 const Header = ({loginData}) => {
   return (
@@ -16,13 +16,24 @@ const Header = ({loginData}) => {
           {
             loginData
               ?
-              <Menu.Item as={NavLink} exact to="/account" position="right">
-                Hello, {loginData.name}
-              </Menu.Item>
+              <Menu.Menu position="right">
+                <Dropdown item text={loginData.email}>
+                  <Dropdown.Menu>
+                    <Dropdown.Item>
+                      <Icon name="sign out"/>Logout
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Menu.Menu>
               :
-              <Menu.Item as={NavLink} exact to="/login" position="right">
-                Login
-              </Menu.Item>
+              <Menu.Menu position="right">
+                <Menu.Item as={NavLink} exact to="/signup">
+                  <Icon name="add user"/>Sign Up
+                </Menu.Item>
+                <Menu.Item as={NavLink} exact to="/login" position="right">
+                  <Icon name="sign in"/>Login
+                </Menu.Item>
+              </Menu.Menu>
           }
         </Menu>
       </Grid.Column>
