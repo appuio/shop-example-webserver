06d093e371fac9089ebef0f52c9663245629fc0a
Roland Schläfli <rolandschlaefli@gmail.com>
2017-03-17 11:46:42 +0100

    Add login component that includes the login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,25 +1,14 @@
 import React from 'react'
-import {Button, Grid, Segment} from 'semantic-ui-react'
-import {reduxForm} from 'redux-form'
+import {Grid, Segment} from 'semantic-ui-react'
+import LoginForm from './LoginForm'
 
-const Login = ({isLoggedIn, login, logout}) => {
-  return (
-    <Grid.Row>
-      <Grid.Column>
-        <Segment>
-          {
-            isLoggedIn
-              ?
-              <Button onClick={(e) => logout()}>Logout</Button>
-              :
-              <Button onClick={(e) => login('rolandschlaefli@gmail.com', 'abcd')}>Login</Button>
-          }
-        </Segment>
-      </Grid.Column>
-    </Grid.Row>
-  )
-}
+const Login = ({loading, login}) =>
+  <Grid.Row>
+    <Grid.Column>
+      <Segment loading={loading}>
+        <LoginForm onSubmit={login}/>
+      </Segment>
+    </Grid.Column>
+  </Grid.Row>
 
-export default reduxForm({
-  form: 'login'
-})(Login)
+export default Login
