4cb8a931980027dff2e73610faf29cf4b890590a
Roland Schläfli <rolandschlaefli@gmail.com>
2017-03-23 20:12:53 +0100

    Add test stubs for login and fetch

--- src/redux/modules/login.test.js
+++ src/redux/modules/login.test.js
@@ -1,6 +1,5 @@
-import reducer from './login'
-
-const jwt = 'JWT'
+import _forIn from 'lodash/forIn'
+import reducer, {loginFail, loginReceive, loginRequest, logout} from './login'
 
 const initialState = {
   loading: false,
@@ -8,10 +7,54 @@ const initialState = {
   error: null
 }
 
+const loggedInState = {
+  loading: false,
+  data: {
+    token: 'JWT'
+  },
+  error: null
+}
+
 describe('login - reducer', () => {
 
   it('should return the initial state', () => {
     expect(reducer(undefined, {})).toEqual(initialState)
+  })
+
+  it('should handle LOGIN_REQUEST', () => {
+    expect(reducer(initialState, loginRequest())).toEqual({
+      loading: true,
+      data: null,
+      error: null
+    })
+  })
+
+  it('should handle LOGIN_RECEIVE', () => {
+    expect(reducer(initialState, loginReceive())).toEqual({
+      loading: false,
+      data: {
+        token: 'JWT'
+      },
+      error: null
+    })
+  })
+
+  it('should handle LOGIN_FAIL', () => {
+    expect(reducer(initialState, loginFail('FAIL'))).toEqual({
+      loading: false,
+      data: null,
+      error: 'FAIL'
+    })
+  })
+
+  it('should handle LOGOUT', () => {
+    _forIn([initialState, loggedInState], state => {
+      expect(reducer(state, logout())).toEqual({
+        loading: false,
+        data: null,
+        error: null
+      })
+    })
   })
 
 })
