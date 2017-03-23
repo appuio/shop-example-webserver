60e761677200f5378dfa9a56e85b9e4d73d1688e
Roland Schläfli <rolandschlaefli@gmail.com>
2017-03-23 19:49:35 +0100

    redux-logger upgrade

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,12 +1,10 @@
 import {applyMiddleware, combineReducers, createStore} from 'redux'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {reducer as formReducer} from 'redux-form'
-import createLogger from 'redux-logger'
+import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import 'whatwg-fetch'
 import * as reducers from '../modules/index'
-
-const logger = createLogger()
 
 export const configureStore = () => {
   return createStore(
