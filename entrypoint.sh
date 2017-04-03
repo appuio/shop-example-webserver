58e0b90f5c4950af1d2f19166de41c8cfe68347e
Roland Schläfli <rolandschlaefli@gmail.com>
2017-04-03 11:00:11 +0200

    Add log output for replace command

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 set -e
 
 # replace the API_URL placeholder with the environment variable
+echo "Replacing API_URL with $API_URL"
 sed -i 's;__API_URL__;'$API_URL';g' /usr/share/nginx/html
 
 >&2 echo "Running webserver..."
