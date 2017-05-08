c43ba60500b87bad4a652451240df9ac5ec60ce5
Roland Schläfli <rolandschlaefli@gmail.com>
2017-05-08 14:53:53 +0200

    Update build script such that application is built in /tmp

--- start-build.sh
+++ start-build.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-npm config set cache /tmp/.npm-cache
+DIR=$(pwd)
+TEMP=/tmp/webserver
+
+# copy the sources to a temporary location
+# as yarn install will fail if started within a shared drive (vbox)
+rm -rf $TEMP
+cp -R $DIR $TEMP
+
+# install the NPM packages in the temporary location
+# and run the build
+cd $TEMP
 yarn install
 yarn run build
+
+# copy the bundled files back to the repository location
+cd $DIR
+rm -rf $DIR/build
+cp -R $TEMP/build $DIR/build
